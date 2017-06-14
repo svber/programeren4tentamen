f89359d023c1a0bdb81fe5bef0dce39ab62fb662
svber <srjberni@avans.nl>
2017-06-14 11:53:55 +0200

    endpoints

---
+++ server/api.js
@@ -0,0 +1,25 @@
+
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+router.get('/login', function(request, response){
+
+});
+
+router.get('/register', function(request, response){
+
+});
+
+router.get('/films?offset=:start&count=:nummer', function(request, response){
+
+});
+
+router.get('/films/:filmid', function(request, response){
+
+});
+
+router.get('/login', function(request, response){
+
+});
+module.exports = router;
